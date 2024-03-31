4c2cc6ec5b99067d2602e01bcd693c4923050223
Jeevan Gaudel <jatingaudel01@gmail.com>
2024-03-31 23:01:24 +1100

    add few more pages and added some click functionality

--- script.js
+++ script.js
@@ -6,3 +6,24 @@
 //   el.style.setProperty("--posX", x - l - w / 2);
 //   el.style.setProperty("--posY", y - t - h / 2);
 // });
+
+// let haha = new Audio(".assests/Microsoft Windows 7 Startup Sound.mp3")
+
+var soud = document.querySelectorAll(".iconClick");
+
+// Add a click event listener to the anchor tag
+soud.forEach(function (press) {
+  press.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var href = press.getAttribute("href");
+
+    var clickSound = document.getElementById("clickSound");
+    clickSound.play();
+
+    setTimeout(function () {
+      // Navigate to the URL specified in the href attribute
+      window.location.href = href;
+    }, 300);
+  });
+});
